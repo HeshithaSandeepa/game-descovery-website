5ba5aa3df436e3cec226a295d10a95089653b7d4
HeshithaSandeepa <hesithedg@gmail.com>
2025-09-08 19:35:50 +0530

    Implement responsive layout using Chakra UI Grid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import Demo from "./Demo"
+import { Grid, GridItem } from "@chakra-ui/react"
+
 
 function App() {
-
-
   return (
     <>
-      <Demo />
+      <Grid templateAreas={{
+        base: `"nav" "main" "footer"`,
+        lg: `"nav nav" "aside main" "footer footer"`
+      }}>
+
+        <GridItem area="nav" bg="red">
+          nav bar
+        </GridItem>
+
+        <GridItem area="aside" bg="green" display={{ base: "none", lg: 'block' }}>
+          Sidebar
+        </GridItem>
+
+        <GridItem area="main" bg="purple">
+          Main
+        </GridItem>
+
+
+        <GridItem area="footer" bg="pink">
+          Footer
+        </GridItem>
+      </Grid >
     </>
   )
 }
