adb71577b99489bbb621ab961c7de348cdc24b3c
HeshithaSandeepa <hesithedg@gmail.com>
2025-11-05 13:51:02 +0530

    Refactor game query state management to use a single GameQuery object

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,54 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react"
-import NavBar from "./components/NavBar"
-import GameGrid from "./components/GameGrid"
-import GenreList from "./components/GenreList"
-import { useEffect, useState } from "react"
-import type { Genre } from "./hooks/useGenre"
-import PlatformSelector from "./components/PlatformSelector"
-import type { Platform } from "./hooks/usePlatforms"
+import { Box, Grid, GridItem } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
+import GameGrid from './components/GameGrid';
+import GenreList from './components/GenreList';
+import { useState } from 'react';
+import type { Genre } from './hooks/useGenre';
+import PlatformSelector from './components/PlatformSelector';
+import type { Platform } from './hooks/usePlatforms';
 
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+}
 
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
-
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+  // const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  // const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
 
   return (
     <>
-      <Grid templateAreas={{
-        base: `"nav" "main" "footer"`,
-        lg: `"nav nav" "aside main" "footer footer"`
-      }}
+      <Grid
+        templateAreas={{
+          base: `"nav" "main" "footer"`,
+          lg: `"nav nav" "aside main" "footer footer"`,
+        }}
         templateColumns={{
           base: '1fr', // base (mobile) වලදී සම්පූර්ණ පළල (1 fraction)
-          lg: '250px 1fr' // lg (laptop/desktop)  aside :250px, main other all width(1 fraction)
+          lg: '250px 1fr', // lg (laptop/desktop)  aside :250px, main other all width(1 fraction)
         }}
       >
-
         <GridItem
           area="nav"
-          position={"sticky"}
+          position={'sticky'}
           top={0}
           zIndex={1}
-          bg={"black"}>
+          bg={'black'}
+        >
           <NavBar />
         </GridItem>
 
         <GridItem
           area="aside"
-          display={{ base: "none", lg: 'block' }}
+          display={{ base: 'none', lg: 'block' }}
           paddingX={5}
           top={0}
-          height={"85vh"}
-          overflowY={"auto"}
+          height={'85vh'}
+          overflowY={'auto'}
           css={{
             //hide scrollbar
-            "&::-webkit-scrollbar": {
+            '&::-webkit-scrollbar': {
               display: 'none',
             },
             msOverflowStyle: 'none',
@@ -51,30 +56,31 @@ function App() {
           }}
         >
           <GenreList
-            onSelectedGenre={(genre: Genre) => setSelectedGenre(genre)} selectedGenre={selectedGenre} />
+            onSelectedGenre={(genre: Genre) =>
+              setGameQuery({ ...gameQuery, genre })
+            }
+            selectedGenre={gameQuery.genre}
+          />
         </GridItem>
 
-        <GridItem area="main" paddingY={5} w={"full"}>
+        <GridItem area="main" paddingY={5} w={'full'}>
           <PlatformSelector
-            selectedPlatform={selectedPlatform}
-            onSelectedPlatform={(platform) => setSelectedPlatform(platform)}
+            selectedPlatform={gameQuery.platform}
+            onSelectedPlatform={(platform) => setGameQuery({ ...gameQuery, platform })}
           />
           <GameGrid
-            selectedGenre={selectedGenre}
-            selectedPlatform={selectedPlatform}
+            gameQuery={gameQuery}
           />
         </GridItem>
 
-
-        <GridItem area="footer" bg="brown" w={"full"}>
-          <Box w={"full"} textAlign={"center"} p={4}>
+        <GridItem area="footer" bg="brown" w={'full'}>
+          <Box w={'full'} textAlign={'center'} p={4}>
             Footer
           </Box>
-
         </GridItem>
-      </Grid >
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
